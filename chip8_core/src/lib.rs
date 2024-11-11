1b898165068a47b424cae9137184c67033ce865f
LucasUTNFRD <ldelgado@frd.utn.edu.ar>
2024-11-11 00:15:51 -0300

    add opcode implementations

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -105,11 +105,150 @@ impl Emu {
         op
     }
 
-    fn execute(opcode: u16) {
+    fn execute(&mut self, opcode: u16) {
         // todo!()
         let digit1 = (opcode & 0xF000) >> 12; // First digit
         let digit2 = (opcode & 0x0F00) >> 8; // Second digit
         let digit3 = (opcode & 0x00F0) >> 4; // Third digit
         let digit4 = opcode & 0x000F; // Fourth digit
+
+        match (digit1, digit2, digit3, digit4) {
+            //NOP
+            (0, 0, 0, 0) => return,
+            //CLS
+            (0, 0, 0xE, 0) => {
+                self.screen = [false; WIDTH * HEIGHT];
+            }
+            //RET
+            (0, 0, 0xE, 0xE) => {
+                let ret_addr = self.pop();
+                self.pc = ret_addr;
+            }
+            //JMP NNN
+            (1, _, _, _) => {
+                let nnn = opcode & 0x0FFF; // 12-bits address
+                self.pc = nnn; // Jump to address
+            }
+            //2NNN - CALL addr
+            (2, _, _, _) => {
+                let nnn = opcode & 0x0FFF; // 12-bits address
+                self.push(self.pc); // add current program counter to stack
+                self.pc = nnn; // Jump to address
+            }
+            //3XNN
+            (3, _, _, _) => {
+                let nn = (opcode & 0x0FF) as u8; // 12-bits address
+                let x = digit2 as usize;
+                if self.v_registers[x] == nn {
+                    self.pc += 2;
+                }
+            }
+            //4XNN
+            (4, _, _, _) => {
+                let nn = (opcode & 0x0FF) as u8; // 12-bits address
+                let x = digit2 as usize;
+                if self.v_registers[x] != nn {
+                    self.pc += 2;
+                }
+            }
+            //5XY0
+            (5, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if self.v_registers[x] == self.v_registers[y] {
+                    //skip next instruction
+                    self.pc += 2;
+                }
+            }
+            //6XNN
+            (6, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (opcode & 0x0FF) as u8; // 12-bits address
+                self.v_registers[x] = nn;
+            }
+            //7XNN
+            //Adds the value NN to register VX.
+            //If the addition results in an overflow,
+            //the value wraps around, and the carry flag (VF) is not affected
+            (7, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (opcode & 0x0FF) as u8; // 12-bits address
+                self.v_registers[x] = self.v_registers[x].wrapping_add(nn);
+            }
+            //8XY0
+            (8, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_registers[y] = self.v_registers[x];
+            }
+            //8XY1
+            (8, _, _, 1) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_registers[y] |= self.v_registers[x];
+            }
+            //8XY2
+            (8, _, _, 2) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_registers[y] &= self.v_registers[x];
+            }
+            //8XY3
+            (8, _, _, 3) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_registers[y] ^= self.v_registers[x];
+            }
+            // 8XY4
+            (8, _, _, 4) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, carry) = self.v_registers[x].overflowing_add(self.v_registers[y]);
+                let new_vf = if carry { 1 } else { 0 };
+
+                self.v_registers[x] = new_vx;
+                self.v_registers[0xF] = new_vf;
+            }
+            // 8XY5
+            (8, _, _, 5) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, borrow) = self.v_registers[x].overflowing_sub(self.v_registers[y]);
+                let new_vf = if borrow { 1 } else { 0 };
+
+                self.v_registers[x] = new_vx;
+                self.v_registers[0xF] = new_vf;
+            }
+            // 8XY6
+            (8, _, _, 6) => {
+                // This operation performs a single right shift on the value in VX, with the bit that was dropped off being stored into the VF register.
+                let x = digit2 as usize;
+                let lsb = self.v_registers[x] & 1;
+                self.v_registers[x] >>= 1;
+                self.v_registers[0xF] = lsb;
+            }
+            // 8XY7
+            (8, _, _, 7) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, borrow) = self.v_registers[y].overflowing_sub(self.v_registers[x]);
+                let new_vf = if borrow { 0 } else { 1 };
+
+                self.v_registers[x] = new_vx;
+                self.v_registers[0xF] = new_vf;
+            }
+            (8, _, _, 0xE) => {
+                let x = digit2 as usize;
+                let msb = (self.v_registers[x] >> 7) & 1;
+                self.v_registers[x] <<= 1;
+                self.v_registers[0xF] = msb;
+            }
+            _ => {
+                todo!();
+            }
+        }
     }
 }
